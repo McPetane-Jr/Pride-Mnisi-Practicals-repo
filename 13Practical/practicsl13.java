ed3fcaade7bfe9cf198730fb081ebbbbdcac2ec2
Pride <mcpetane@gmail.com>
2026-02-23 15:19:30 +0200

    Created the binary search method

--- 13Practical/practicsl13.java
+++ 13Practical/practicsl13.java
@@ -13,5 +13,23 @@ public class practicsl13 {
         return -1; 
     }
 
+    // Binary search method
+    public static int binarySearch(Node[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+        
+        while (left <= right) {
+            int mid = (left + (right - left)) / 2;
+            
+            if (array[mid].key == key) {
+                return mid; 
+            } else if (array[mid].key < key) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return -1; 
+    }
    
 }
