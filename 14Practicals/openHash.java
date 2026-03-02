190cf59ad969d2a072d40442fa7e043de6c9623b
Pride <mcpetane@gmail.com>
2026-03-02 16:19:31 +0200

    Implemted openHashing methods such as insert, delete and search in the openHash.java file. The code is now ready to be committed to the repository.

--- 14Practicals/openHash.java
+++ 14Practicals/openHash.java
@@ -15,25 +15,71 @@ public class openHash {
             this.key = key;
             this.value = value;
         }
+    }
 
-        private Node[] table; // This will be our hash table
-        private int m; // Size of the hash table
-        private int size; // Number of key-value pairs in the hash table
+    private Node[] table; // This will be our hash table
+    private int m; // Size of the hash table
+    private int size; // Number of key-value pairs in the hash table
 
-        public openHash(int m){
+    public openHash(int m){
 
-            this.m = m;
+        this.m = m;
 
-            this.table = new Node[m + 1]; 
-            this.size = 0;
+        this.table = new Node[m + 1]; 
+        this.size = 0;
+    }
+
+    private int hash(String key) {
+        int hashValue = Math.abs(key.hashCode());
+        return (hashValue % m) + 1;   
+    }
+
+    public void insert(String key, String value) { 
+
+        if (size == m) {
+            return;   // table full
         }
 
-        private int hash(String key) {
-            int hashValue = Math.abs(key.hashCode());
-            return (hashValue % m) + 1;   
+        int i = hash(key);
+
+        while (table[i] != null) {
+
+        // If key already exists, update value
+        if (table[i].key.equals(key)) {
+            table[i].value = value;
+            return;
         }
 
+        i = (i % m) + 1;  // linear probing
+    }
+
+    table[i] = new Node(key, value);
+    size++;
         
             
+    }
+
+    public String search(String key) {
+
+        int i = hash(key);
+
+        while (table[i] != null) {
+
+            if (table[i].key.equals(key)) {
+                return table[i].value; // Key found, return value
+            }
+
+            i = (i % m) + 1; // linear probing
+        }
+
+        return null; // Key not found
+    }
+
+    public Boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Boolean isFull() {
+        return size == m;
     }
 }
