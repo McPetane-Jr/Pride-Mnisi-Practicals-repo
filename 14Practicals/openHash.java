84cd805c5cec0644c097b4d6f3b5ab6511cc1e80
Pride <mcpetane@gmail.com>
2026-03-02 16:05:41 +0200

    "Added open hash implementation"

--- 14Practicals/openHash.java
+++ 14Practicals/openHash.java
@@ -6,8 +6,20 @@ public class openHash {
     // yields an integer index i ∈ [1..m] that has a random uniform distribution. Note N 
     // the number of key–value data pairs is smaller than m. If i has a biased distribution 
     // then the hash function hash(key) will flop.
-    public static void main(String[] args) {
-        
-       
-}
+
+    private static class Node{
+        String key;
+        String value;
+
+        Node(String key, String value){
+            this.key = key;
+            this.value = value;
+        }
+
+        private Node[] table; // This will be our hash table
+        private int m; // Size of the hash table
+        private int size; // Number of key-value pairs in the hash table
+
+        pu
+    }
 }
