0c59f360222959618a10224488e5fa8897003af5
Pride <mcpetane@gmail.com>
2026-03-16 15:10:55 +0200

    We've just built a max heap from the input array using the bottom-up approach

--- 16Practicals/tryHeapsort.java
+++ 16Practicals/tryHeapsort.java
@@ -1,5 +1,31 @@
 
 
 public class tryHeapsort {
+
+    static String[] heap;
+    static int size;
+
+    static void bottomUpHeap(String[] arr) {
+
+        heap = arr.clone(); //cloning so that we don't modify the original array
+        size = heap.length;
+
+        int lastParent = (size - 2) / 2; //index of the last parent node
+
+        //Moving from the last parent node to the root node and heapifying each node
+        for (int i = lastParent; i >= 0; i--) {
+            heapifyDown(i);
+        }
+
+    } 
+
+    private static void heapifyDown(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'heapifyDown'");
+    }
+
+
+
+
     
 }
